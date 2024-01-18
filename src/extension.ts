0a607ae7e699658725b932e6937d65fe83e7428d
NachoPal <ignacio.palacios.santos@gmail.com>
2024-01-18 20:23:16 +0100

    on the way of querying metadata

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -21,7 +21,84 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from ParachainsIntegrationTests!');
 	});
 
-	context.subscriptions.push(disposable);
+  let loadChainSpecsOnOpen = vscode.workspace.onDidOpenTextDocument(loadChainsMetadata);
+  let loadChainSpecsOnChange = vscode.workspace.onDidChangeTextDocument(e => loadChainsMetadata(e.document));
+
+  let palletAndCallSuggestions = vscode.languages.registerCompletionItemProvider(
+    'yaml', // This will make the provider work only for yaml files
+    {
+      provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+
+      // Get the range from the start of the line to the current position
+      const range = new vscode.Range(position.line, 0, position.line, position.character);
+
+      // Get the text in this range
+      const text = document.getText(range);
+
+      // Split the text into words
+      const words = text.split(/\s+/).filter(word => word !== '');
+
+      // Get the last word
+      const wordAfterIdentifier = words[words.length - 1];
+
+      // Key identifier
+      const identifier = words[0];
+
+      let suggestions = undefined;
+
+      // Only when we want to add a pallet or call
+      if (identifier !== 'pallet:' && identifier !== 'call:') {
+          return undefined;
+      } else if (identifier === 'pallet:') {
+        // Offer available pallets
+        // Compute completion items based on the word before the cursor
+        suggestions = computeCompletionItems(wordAfterIdentifier);
+      } else if (identifier === 'call:') {
+        // Look for the selected pallet and offer available calls
+        // Compute completion items based on the word before the cursor
+        suggestions = computeCompletionItems(wordAfterIdentifier);
+      }
+
+      return suggestions;
+    }
+    },
+    ...'abcdefghijklmnopqrstuvwxyz'.split(''), // Trigger the provider whenever any letter is typed
+    // '*', // Trigger the provider whenever '*' is typed
+    // '$' // Trigger the provider whenever '$' is typed
+    );
+
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(loadChainSpecsOnOpen, loadChainSpecsOnChange);
+    context.subscriptions.push(palletAndCallSuggestions);
+
+    // If there is an active document when the extension is activated, parse it
+    if (vscode.window.activeTextEditor) {
+      loadChainsMetadata(vscode.window.activeTextEditor.document);
+    }
+}
+
+function loadChainsMetadata(document: vscode.TextDocument) {
+  if (document.languageId !== 'yaml') {
+      return;
+  }
+
+  let text = document.getText();
+  let regex = /(\w+):\s*&(\w+)\s*\n\s*wsPort:\s*(\d+)/g;
+  let match;
+
+  while ((match = regex.exec(text)) !== null) {
+      let chainName = match[1];
+      let referenceName = match[2];
+      let wsPortValue = match[3];
+
+      vscode.window.showInformationMessage(`Found wsPort in chain '${chainName}' with reference '${referenceName}' and value '${wsPortValue}'`);
+  }
+}
+
+function computeCompletionItems(wordBeforeCursor: string): vscode.CompletionItem[] {
+  const item = new vscode.CompletionItem(`You typed '${wordBeforeCursor}'`, vscode.CompletionItemKind.Method);
+  item.sortText = '!'; // This will sort before any string that doesn't start with a '0'
+  return [item, item];
 }
 
 // This method is called when your extension is deactivated
